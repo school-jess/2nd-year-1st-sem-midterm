f35d8e13717c5a1c0128253a408972e713e3c4d7
invalid-code <jessgaming55@gmail.com>
2025-08-31 12:09:25 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace OOPMidterm;
+
+interface IId
+{
+    public int id;
+}
+
+class SinglyLinkedList<T>
+{
+    public SinglyLinkedList? Next;
+    private T data;
+}
+
+class Student : IId
+{
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello");
+    }
+}
